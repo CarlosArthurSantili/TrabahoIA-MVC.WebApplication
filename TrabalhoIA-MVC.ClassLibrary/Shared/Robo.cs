473953f18fcb163eed0e39521721a229fa4e96b4
Carlos Arthur Santili <74028702+CarlosArthurSantili@users.noreply.github.com>
2021-10-22 09:20:50 -0300

    Add project files.

---
+++ TrabalhoIA-MVC.ClassLibrary/Shared/Robo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoIA_MVC.ClassLibrary.Shared
+{
+    public class Robo
+    {
+        string nome;
+        Coordenada localizacao;
+        
+        public Robo(string nome, Coordenada localizacao) 
+        {
+            this.nome = nome;
+            this.localizacao = localizacao;
+        }
+    }
+}
